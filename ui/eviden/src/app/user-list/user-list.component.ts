3b7e9995dee25237b2167ae2faab2a52f35e2caf
csoto <pontiacsb@gmail.com>
2023-10-13 05:01:09 -0700

    WIP: handle errors during data fetching, done

--- ui/eviden/src/app/user-list/user-list.component.ts
+++ ui/eviden/src/app/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../models/User.model';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
@@ -12,11 +13,21 @@ export class UserListComponent implements OnInit {
   searchTerm: string = '';
   sortOrder: 'asc' | 'desc' = 'asc';
   sortCriterion: 'name' | 'email' = 'name';
+  errorMessage: string = '';
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((users) => (this.users = users));
+    this.userService
+      .getUsers()
+      .pipe(
+        catchError((err) => {
+          console.error(err);
+          this.errorMessage = err.message;
+          return of([]);
+        })
+      )
+      .subscribe((users) => (this.users = users));
   }
   toggleSortOrder() {
     this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
